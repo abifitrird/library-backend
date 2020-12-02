67c48b23571deb255d59107c8527bb2cccd0ff6e
abifitrird <fratnadewi@gmail.com>
2020-12-02 21:06:25 +0700

    edit file migration for User table

--- migrations/20201010171243-create-user.js
+++ migrations/20201010171243-create-user.js
@@ -26,6 +26,9 @@ module.exports = {
       address: {
         type: Sequelize.STRING,
       },
+      photo: {
+        type: Sequelize.STRING,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
